a4a22c7120a55833152a5bc6368431c7ecd7fe55
Martijn Reeuwijk <martijn.reeuwijk@kickorstick.com>
2015-10-23 16:04:53 +0200

    removed insecure

--- client/￼￼templates/posts/post_submit.js
+++ client/￼￼templates/posts/post_submit.js
@@ -1,16 +1,10 @@
 Template.postSubmit.events({
-  'submit form':function(e){
+  'submit form': function(e) {
     e.preventDefault();
-
     var post = {
-      url:$(e.target).find('[name=url]').val(),
-      title:$(e.target).find('[name=title]').val()
+      url: $(e.target).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
     };
-    Meteor.call('postInsert', post,function(error,result){
-      if (error)
-        return alert(error.reason);
-
-        Router.go('postPage',{_id:result._id});
-    });
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
   }
 });
