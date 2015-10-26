a1692b8d76ae399057d62cfecc8ce7403b2a4064
Martijn Reeuwijk <martijn.reeuwijk@kickorstick.com>
2015-10-26 14:29:41 +0100

    Na de "fix"

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,30 +1,31 @@
 Template.postEdit.events({
-  'submit form': function(e){
+  'submit form': function(e) {
     e.preventDefault();
-
+    
     var currentPostId = this._id;
-
+    
     var postProperties = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
-
-    Post.update(currentPostId), {$set: postProperties}, function(error){
-      if (error){
+    
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
         alert(error.reason);
-      }else {
+      } else {
         Router.go('postPage', {_id: currentPostId});
-
       }
-    };
+    });
   },
-
-  'click .delete':function(e){
+  
+  'click .delete': function(e) {
     e.preventDefault();
-    if(confirm("Are you sure you want to delete")){
+    
+    if (confirm("Delete this post?")) {
       var currentPostId = this._id;
-      posts.remove(currentPostId);
+      Posts.remove(currentPostId);
       Router.go('postsList');
     }
   }
-});//laatste
+});
