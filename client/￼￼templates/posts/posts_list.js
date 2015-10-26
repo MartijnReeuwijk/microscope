9d28b1510d479fa49d18d61b6a71d204cdec39a5
Martijn Reeuwijk <martijn.reeuwijk@kickorstick.com>
2015-10-26 12:22:31 +0100

    extra

--- client/￼￼templates/posts/posts_list.js
+++ client/￼￼templates/posts/posts_list.js
@@ -18,6 +18,6 @@ Template.postsList.helpers({
 
 Template.postsList.helpers({
   posts:function(){
-    return Posts.find({author:'bob-smith',category:'JavaScript'});
+    return Posts.find({},{sort:{submitted: -1}});
   }
 });
